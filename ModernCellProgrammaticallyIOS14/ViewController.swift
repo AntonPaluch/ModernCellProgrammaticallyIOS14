f40f86b2f813d14e4dc0f7be82dc4021325c25ba
Dmitry Pavlov <perr07@yandex.ru>
2021-11-26 20:50:53 +0500

    Initial Commit

---
+++ ModernCellProgrammaticallyIOS14/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ModernCellProgrammaticallyIOS14
+//
+//  Created by Pandos on 26.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
